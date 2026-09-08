02a22ca68b5d1ed46333e83efe2a7bb1ab3b3e97
yoroki12 <kevinjonathanhendrawan@gmail.com>
2026-09-08 21:53:26 +0700

    feat: add device database

--- scanner.py
+++ scanner.py
@@ -1,7 +1,15 @@
 from scapy.all import ARP, Ether, srp
+import json
 
-NETWORK = "192.168.18.0/24"
+NETWORK = input("ip= ")
 INTERFACE = "wlan0"
+
+with open("devices.json", "r") as file:
+    devices = json.load(file)
+
+known_devices = devices["devices"]
+
+print(known_devices)
 
 packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=NETWORK)
 
@@ -16,7 +24,22 @@ print("\nDevices found:")
 print("-" * 50)
 
 for _, response in answered:
-    print(f"IP: {response.psrc:<16} MAC: {response.hwsrc}")
+
+    mac = response.hwsrc
+    ip = response.psrc
+
+    if mac not in known_devices:
+        known_devices[mac] = {
+            "ip": ip
+        }
+
+    print(f"IP: {ip:<16} MAC: {mac}")
+
+with open("devices.json", "w") as file:
+    json.dump(devices, file, indent=4)
+
+print("\nDatabase in memory:")
+print(known_devices)
 
 print("-" * 50)
 print(f"Total devices: {len(answered)}")
